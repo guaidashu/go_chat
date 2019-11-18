e6d85948e3e4f31fccde3cd68fa3b02e9a355436
yy <a1023767856@163.com>
2019-11-18 11:38:53 +0800

    add model extends

--- origin/model/base.go
+++ origin/model/base.go
@@ -2,7 +2,15 @@ package model
 
 import (
 	"github.com/go-xorm/xorm"
+	"time"
 )
+
+type Model struct {
+	Id      int64     `xorm:"pk autoincr bigint(20)"` // 用户的id
+	Status  int       `xorm:"int(1) default 1"`
+	Created time.Time `xorm:"created"`
+	Updated time.Time `xorm:"updated"`
+}
 
 type BaseModel interface {
 	TableName() string
--- origin/model/user.go
+++ origin/model/user.go
@@ -3,7 +3,6 @@ package model
 import (
 	"github.com/go-xorm/xorm"
 	"go_chat/app/libs"
-	"time"
 )
 
 const (
@@ -13,19 +12,16 @@ const (
 )
 
 type UserModel struct {
-	Id       int64     `xorm:"pk autoincr bigint(20)"`                       // 用户的id
-	Mobile   string    `json:"mobile" xorm:"varchar(255)" form:"mobile"`     // 手机号码
-	Passwd   string    `json:"passwd" xorm:"varchar(255)" form:"passwd"`     // 用户密码 = f(plainpwd + salt), MD5
-	Avatar   string    `json:"avatar" xorm:"varchar(255)" form:"avatar"`     // 头像
-	Sex      string    `json:"sex" xorm:"varchar(2)" form:"sex"`             //
-	Nickname string    `json:"nickname" xorm:"varchar(255)" form:"nickname"` //
-	Salt     string    `json:"salt" xorm:"varchar(10)" form:"salt"`          // 加密随机数
-	Online   int       `json:"online" xorm:"int(10)" form:"online"`          //
-	Token    string    `json:"token" xorm:"varchar(255)" form:"token"`       // /chat?id=1&token=x
-	Memo     string    `json:"memo" xorm:"varchar(140)" form:"memo"`         //
-	Status   int       `xorm:"int(1) default 1"`
-	Created  time.Time `xorm:"created"`
-	Updated  time.Time `xorm:"updated"`
+	Model    `xorm:"extends"`
+	Mobile   string `json:"mobile" xorm:"varchar(255)" form:"mobile"`     // 手机号码
+	Passwd   string `json:"passwd" xorm:"varchar(255)" form:"passwd"`     // 用户密码 = f(plainpwd + salt), MD5
+	Avatar   string `json:"avatar" xorm:"varchar(255)" form:"avatar"`     // 头像
+	Sex      string `json:"sex" xorm:"varchar(2)" form:"sex"`             //
+	Nickname string `json:"nickname" xorm:"varchar(255)" form:"nickname"` //
+	Salt     string `json:"salt" xorm:"varchar(10)" form:"salt"`          // 加密随机数
+	Online   int    `json:"online" xorm:"int(10)" form:"online"`          //
+	Token    string `json:"token" xorm:"varchar(255)" form:"token"`       // /chat?id=1&token=x
+	Memo     string `json:"memo" xorm:"varchar(140)" form:"memo"`         //
 }
 
 func (u *UserModel) GetDB() *xorm.Session {
